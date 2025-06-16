c2d85a5fafffbb278dc5f9cd3426fb4db2a3ed17
Conor Deegan <conorjdeegan@gmail.com>
2025-06-16 14:32:30 +0100

    move fixtures to new file

--- src/prove.rs
+++ src/prove.rs
@@ -536,7 +536,7 @@ mod tests {
     use super::*;
     use crate::config::{Environment, tests::test_config};
 
-    use crate::end_to_end_tests::*;
+    use crate::fixtures::*;
     use crate::utils::tests::TURNSTILE_TEST_SECRET_KEY_ALWAYS_BLOCKS;
 
     use ml_dsa::{KeyGen, signature::Signer};
